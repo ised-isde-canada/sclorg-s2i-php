a8289c572aa0d9379c4bf36f61b7a4fefaa38a46
Duncan Sutter <dsutter@qualivera.com>
2020-07-06 13:53:13 -0400

    Fix pgpass issue

--- backup-restore/backup.php
+++ backup-restore/backup.php
@@ -1,14 +1,20 @@
 <?php
+
 ini_set('error_reporting', E_ALL & ~E_NOTICE & ~E_STRICT & ~E_DEPRECATED);
 
 $HOME = getenv('HOME');
-$SITES = '/opt/app-root/src/html/sites';
+$SITES = "$HOME/html/sites";
 
-// TODO: Put site into maintenance mode
+putenv("PGPASSFILE=$HOME/.pgpass");
 
+// enter maintenance mode
+echo "Entering maintenance mode...\n";
+exec("$HOME/vendor/bin/drush state:set system.maintenance_mode 1 --input-format=integer");
+exec("$HOME/vendor/bin/drush cr");
+
+// check for s3 credentials configuration file
 $s3ini = parse_ini_file("$HOME/.s3cfg", false, INI_SCANNER_RAW);
 $bucket = $s3ini['host_bucket'];
-
 if (empty($bucket)) {
     echo "No S3 bucket defined!\n";
     exit;
@@ -22,6 +28,7 @@ $db_pass = $databases['default']['default']['password'];
 $db_host = $databases['default']['default']['host'];
 $db_port = $databases['default']['default']['port'];
 
+// create .pgpass file
 $pgpass = "$db_host:$db_port:$db_name:$db_user:$db_pass";
 if ($fp = fopen("$HOME/.pgpass", "w")) {
   fwrite($fp, "$pgpass\n");
@@ -66,3 +73,9 @@ echo "$cmd\n";
 
 // Remove the pgpass file
 unlink("$HOME/.pgpass");
+
+// exit maintenance mode
+echo "Exiting maintenance mode...\n";
+exec("$HOME/vendor/bin/drush state:set system.maintenance_mode 0 --input-format=integer");
+exec("$HOME/vendor/bin/drush cr");
+
