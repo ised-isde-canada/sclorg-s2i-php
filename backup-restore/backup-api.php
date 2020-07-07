f5766144261d4d5ca04f2af36dfe36642d018ae9
Duncan Sutter <dsutter@qualivera.com>
2020-07-07 06:23:14 -0400

    backup api script to output json

--- backup-restore/backup-api.php
+++ backup-restore/backup-api.php
@@ -6,7 +6,7 @@ $HOME = getenv('HOME');
 $SITES = "$HOME/html/sites";
 
 $pgpassfile = "$HOME/.pgpass";
-$s3config = "$HOME/.s3cfg";
+$s3cfgfile = "$HOME/.s3cfg";
 
 putenv("PGPASSFILE=$pgpassfile");
 
@@ -22,11 +22,11 @@ $s3data = str_replace('__SECRET_KEY__', $secret_key, $s3data);
 $s3data = str_replace('__BUCKET_LOCATION__', $bucket_location, $s3data);
 $s3data = str_replace('__HOST_BASE__', $host_base, $s3data);
 $s3data = str_replace('__HOST_BUCKET__', $host_bucket, $s3data);
-file_put_contents($s3config, $s3data);
-chmod($s3config, 0600);
+file_put_contents($s3cfgfile, $s3data);
+chmod($s3cfgfile, 0600);
 
 // enter maintenance mode
-echo "Entering maintenance mode...\n";
+$json['messages'][] = "Entering maintenance mode...\n";
 exec("$HOME/vendor/bin/drush state:set system.maintenance_mode 1 --input-format=integer");
 exec("$HOME/vendor/bin/drush cr");
 
@@ -52,39 +52,43 @@ $tarfile = "drupal-$dtm.tar";
 
 // Dump using tar format (-F t)
 $cmd = "pg_dump -U $db_user -h $db_host -p $db_port -x -F t $db_name > /tmp/$dbbackup";
-echo "$cmd\n";
+$json['messages'][] = $cmd;
 `$cmd`;
 
 chdir('/tmp');
 $cmd = "tar cf /tmp/$tarfile $dbbackup";
-echo "$cmd\n";
+$json['messages'][] = $cmd;
 `$cmd`;
 
 chdir("$SITES/..");
 $cmd = "tar rf /tmp/$tarfile sites";
-echo "$cmd\n";
+$json['messages'][] = $cmd;
 `$cmd`;
 
 $cmd = "gzip -f /tmp/$tarfile";
-echo "$cmd\n";
+$json['messages'][] = $cmd;
 `$cmd`;
 
 $cmd = "s3cmd -q --mime-type=application/x-gzip put /tmp/$tarfile.gz s3://$host_bucket/$tarfile.gz";
-echo "$cmd\n";
-`$cmd`;
+$json['messages'][] = $cmd;
+$result = `$cmd`;
+$json['messages'][] = "Result from s3cmd is: $result";
 
 $cmd = "rm -f /tmp/$dbbackup";
-echo "$cmd\n";
+$json['messages'][] = $cmd;
 `$cmd`;
 
 $cmd = "rm -f /tmp/$tarfile.gz";
-echo "$cmd\n";
+$json['messages'][] = $cmd;
 `$cmd`;
 
-// Remove the pgpass file
-unlink("$HOME/.pgpass");
+unlink($pgpassfile);
+unlink($s3cfgfile);
 
 // exit maintenance mode
-echo "Exiting maintenance mode...\n";
+$json['messages'][] = "Exiting maintenance mode...\n";
 exec("$HOME/vendor/bin/drush state:set system.maintenance_mode 0 --input-format=integer");
 exec("$HOME/vendor/bin/drush cr");
+
+header('Content-type: application/json; charset=utf-8');
+echo json_encode($json);
